c1e899ed8defa644d2e148000cd32228c36f34d4
Ali <a.a.davudov@yandex.ru>
2022-04-18 10:10:41 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let number = 5;
+const leftBorderWidth =1;
+
+number = 10;
+console.log(number);
+console.log(name);
+var name = 'Ivan';
+
+const obj = { 
+  name: 'John',
+  age: 25,
+  isMarried: false
+
+}
+
+//
+
+//const answer = prompt('Вам есть 18?', '18');
+////console.log(typeof(answer));
+
+const ansvers = [];
+ansvers[0] = promt("Как ваше имя?", "");
+ansvers[1] = promt("Как ваша фамилия?", "");
+ansvers[2] = promt("Как ваше отчество?", "");
+
+document.write(ansvers);
