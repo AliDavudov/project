50078e62e28aedd45326fc23b3a2846b6b0ca7ff
Ali <a.a.davudov@yandex.ru>
2022-04-18 13:21:20 +0400

    1 level

--- js/script.js
+++ js/script.js
@@ -1,26 +1,16 @@
-let number = 5;
-const leftBorderWidth =1;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
 
-number = 10;
-console.log(number);
-console.log(name);
-var name = 'Ivan';
-
-const obj = { 
-  name: 'John',
-  age: 25,
-  isMarried: false
-
-}
-
-//
-
-//const answer = prompt('Вам есть 18?', '18');
-////console.log(typeof(answer));
-
-const ansvers = [];
-ansvers[0] = promt("Как ваше имя?", "");
-ansvers[1] = promt("Как ваша фамилия?", "");
-ansvers[2] = promt("Как ваше отчество?", "");
-
-document.write(ansvers);
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d; 
+      console.log(personalMovieDB);  
